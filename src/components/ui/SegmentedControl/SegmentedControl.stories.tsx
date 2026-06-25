1bc449e4ef760b06499020d5c579810f3af9cba5
KyoheiTashiro <kyokyo_dc5@yahoo.co.jp>
2026-06-25 15:33:55 +0900

    Revert "test"
    This reverts commit d87ad204fa330628b666112fd0d031a5b1206929.

--- src/components/ui/SegmentedControl/SegmentedControl.stories.tsx
+++ src/components/ui/SegmentedControl/SegmentedControl.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { useState } from "react";
 
-import { SegmentedContro } from "./SegmentedControl";
+import { SegmentedControl } from "./SegmentedControl";
 
 const options = [
   { value: "xsmall", label: "極小" },
